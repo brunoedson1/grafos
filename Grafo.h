4eceda1bf8632f9659ba51a4f89e3067ee43b65f
Bruno Alves <108815343+brunoedson1@users.noreply.github.com>
2023-07-07 10:09:18 -0300

    Update Grafo.h
    funções adicionadas

--- Grafo.h
+++ Grafo.h
@@ -2,6 +2,7 @@
 #define GRAFO_H_INCLUDED
 #include "No.h"
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -19,6 +20,10 @@ class Grafo{
         void insereNoInicio(int id);
         void insereNoFim(int id);
         void insereAresta(int id_cauda, int id_cabeca, float peso);
+        vector<No> listaNos();
+        vector<int> guloso();
+        vector<int> randomizado(float,int);
+        vector<int> reativo(float,int);
         
         bool getDigrafo();
         int getGrauNo(int id); 
@@ -39,11 +44,27 @@ class Grafo{
         No *encontrarNo(int id);
         int *seqDeGraus();
 
-        vector<int> dijkstra(int id_calda, int id_cabeca);
-        vector<int> floydWarshall(int id_calda, int id_cabeca);
+        //vector<int> dijkstra(int id_calda, int id_cabeca);
+        void caminhoMinimoFloyd(int origem, int destino);
+        void caminhoMinimoDijkstra(int origem, int destino);
 
         void vizinhancaAberta(int id);
         void vizinhancaFechada(int id);
+        bool independente(vector<No>);
+        bool verificaVisit(bool visitados[], int n);
+        Grafo subgrafoInduzido(const vector<int> &conjuntoVertices);
+        void componentesFortementeConexas();
+        void dfsOrdenacaoTopologica(No* no, vector<bool> &visitados, stack<int> &pilha);
+        void dfsComponenteFortementeConexa(Grafo &grafo, int vertice, vector<bool> &visitados, vector<int> &componente);
+
+        bool conectado();
+        bool fortementeConectado();
+        void dfsAlcancavel(Grafo &grafo, int vertice, vector<bool> &visitados);
+        bool mesmoComponenteConexaoFraca();
+        bool euleriano();
+
+        vector<No*> getFechoTransitivoDireto(int idNo);
+        vector<No*> getFechoTransitivoIndireto(int idNo);
 };
 
 #endif // GRAFO_H_INCLUDED
